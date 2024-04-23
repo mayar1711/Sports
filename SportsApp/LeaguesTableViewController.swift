729edf8524534ff80a5f1f3814e03fcded5d3c48
mayar1711 <mayarhassan1711@gmail.com>
2024-04-23 20:43:52 +0200

    add

--- SportsApp/LeaguesTableViewController.swift
+++ SportsApp/LeaguesTableViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import Alamofire
 
 class LeaguesTableViewController: UITableViewController {
 
     var selectedSport :String?
-    
+    var leagues: [League] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+
+    func fetchData(for sport: String) {
+        let urlString = "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=130041c3994aa5c5e9a427b128c4e48be1235ae9e3b98bccb25f971282dfcff3"
+        
+    
+        AF.request(urlString).responseJSON { (response: DataResponse<Any, AFError>) in
+            switch response.result {
+            case .success(let value):
+                if let json = value as? [String: Any], let leaguesData = json["result"] as? [[String: Any]] {
+                    self.leagues = leaguesData.compactMap { League(json: $0) }
+                    self.tableView.reloadData()
+                    
+                    print(self.leagues)
+
+                }
+            case .failure(let error):
+                print("Error fetching data: \(error)")
+            }
+        }
     }
 
     // MARK: - Table view data source
