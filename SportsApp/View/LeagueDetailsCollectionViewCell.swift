cc70134efc2663fa863e558585aed4f1f20ed93c
mayar1711 <mayarhassan1711@gmail.com>
2024-04-26 12:59:27 +0300

    create LeagueDetails view

---
+++ SportsApp/View/LeagueDetailsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueDetailsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Mayar on 26/04/2024.
+//
+
+import UIKit
+
+class LeagueDetailsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
