083957ca16a4a70d2132aac40a2d004e40a03061
Shimaasami400 <114086723+Shimaasami400@users.noreply.github.com>
2024-04-30 17:16:12 +0300

    handling adding items to favorite

--- SportsApp/leaguedetail/view/LeagueDetailsCollectionViewController.swift
+++ SportsApp/leaguedetail/view/LeagueDetailsCollectionViewController.swift
@@ -130,9 +130,18 @@ class LeagueDetailsCollectionViewController: UICollectionViewController,LeagueDe
         print(lastLeagueDetails?.count)
         collectionView.reloadData()
     }
-    //add alert
+    
     func showFavoriteLeagueSavedSuccess() {
-            print("done")
+        let alert = UIAlertController(title: "Added to Favorites", message: "", preferredStyle: .alert)
+                
+                let titleFont = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.systemGreen]
+                let titleAttrString = NSMutableAttributedString(string: "Added to Favorites", attributes: titleFont)
+                alert.setValue(titleAttrString, forKey: "attributedTitle")
+                
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                
+                present(alert, animated: true, completion: nil)
         }
     
     func showError(message: String) {
